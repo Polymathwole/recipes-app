4fa3278b70647884eabcc65d55a1ba73ff633c76
Oluwaremi Oluwole <polymathwole@gmail.com>
2018-11-05 05:34:42 +0100

    Add components

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../../models/recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [new Recipe('A recipe', 'This is simply a test', 'https://media.defense.gov/2018/Jul/23/2001945968/780/780/0/180717-F-YE685-1062.JPG')];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
